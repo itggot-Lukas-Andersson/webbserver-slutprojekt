a6cdbc298e89cc4d780b238e6775fb7221cad237
itggot-Lukas-Andersson <lukas.andersson1@itggot.se>
2018-04-26 17:42:27 +0200

    Comments now show up on correct posts

--- app.rb
+++ app.rb
@@ -3,26 +3,27 @@ require 'sqlite3'
 
 get('/') do
 	db = SQLite3::Database.new("./db/forum.db")
-	all = db.execute('SELECT * FROM posts ORDER BY id DESC LIMIT 5')
+    all = db.execute('SELECT * FROM post ORDER BY id DESC LIMIT 5')
 	erb(:index, locals:{posts:all})
 end
 
 get('/view/:id') do
 	db = SQLite3::Database.new("./db/forum.db")
 	id = params[:id]
-	post = db.execute('SELECT * FROM posts WHERE id IS ' + id)
-	erb(:view, locals:{posts:post})
+    post = db.execute('SELECT * FROM post WHERE id IS ' + id)
+    comments = db.execute('SELECT * FROM comment WHERE post IS ' + id)
+	erb(:view, locals:{posts:post, comments:comments})
 end
 
 get('/hot') do
 	db = SQLite3::Database.new("./db/forum.db")
-	all = db.execute('SELECT * FROM posts ORDER BY points DESC LIMIT 5')
+	all = db.execute('SELECT * FROM post ORDER BY points DESC LIMIT 5')
 	erb(:index, locals:{posts:all})
 end
 
 get('/title') do
 	db = SQLite3::Database.new("./db/forum.db")
-	all = db.execute('SELECT * FROM posts ORDER BY title COLLATE NOCASE LIMIT 5')
+	all = db.execute('SELECT * FROM post ORDER BY title COLLATE NOCASE LIMIT 5')
 	erb(:index, locals:{posts:all})	
 end
 
